3c7ed58261eafdcdde193afed3bd493975a9bd33
Abrar Sian <abrarsian96@gmail.com>
2024-09-25 10:49:03 +0600

    history added

--- js folder/function.js
+++ js folder/function.js
@@ -3,3 +3,8 @@ function getTheMoneyAmount(id){
     const numericValue = Number(man.replace(/\D/g, ''))
     return numericValue;
 }
+function showSectionById(id){
+    document.getElementById('donation-section').classList.add('hidden');
+    document.getElementById('history-section').classList.add('hidden');
+    document.getElementById(id).classList.remove('hidden');
+}
--- js folder/home-page.js
+++ js folder/home-page.js
@@ -1,3 +1,17 @@
+document.getElementById('donationBtn').addEventListener('click',function(){
+    document.getElementById('historyBtn').classList.remove('active',true);
+    document.getElementById('donationBtn').classList.add('default',true);
+    showSectionById('donation-section');
+}
+)
+
+document.getElementById('historyBtn').addEventListener('click',function(){
+    document.getElementById('donationBtn').classList.remove('default');
+    document.getElementById('historyBtn').classList.add('active');
+    showSectionById('history-section');
+}
+)
+
 document.getElementById('button-clicker').addEventListener('click',function(event){
     event.preventDefault();
     let addingMoney = document.getElementById('donate1').value;
@@ -22,6 +36,15 @@ window.addEventListener('click', (event) => {
         document.getElementById('balance').innerHTML = `<p id="balance" class="flex items-center font-bold"><img class="pr-1" src="./images/coin.png" alt="">${remainingBalance} BDT</p>`;
         modal.classList.remove('hidden');
 
+        const div= document.createElement('div');
+        div.classList.add('border-solid','border-2','border-black','mb-4','p-3');
+        const uniqueid = 'current-date-' + Date.now();
+        div.innerHTML=`
+        <h4 class="text-2xl font-bold">${addingMoney} is donated for flood at Noakhali,Bangladesh</h4>
+        <p>Date:<span id="${uniqueid}"></span></p>
+        `
+        document.getElementById('history-container').appendChild(div);
+        document.getElementById(uniqueid).innerText = new Date().toString();
 }
     else{
         alert("Please Provide us your donate, not your words or funny number");
@@ -49,6 +72,16 @@ document.getElementById('button-clicker1').addEventListener('click',function(eve
     document.getElementById('total-donate1').innerHTML = `<a href="" id="total-donate" class="pb-4 font-bold flex"><img class="pr-1" src="./images/coin.png" alt="">${finalDonate} BDT</a>`;
     document.getElementById('balance').innerHTML = `<p id="balance" class="flex items-center font-bold"><img class="pr-1" src="./images/coin.png" alt="">${remainingBalance} BDT</p>`;
     modal.classList.remove('hidden');
+
+    const div= document.createElement('div');
+        div.classList.add('border-solid','border-2','border-black','mb-4','p-3');
+        const uniqueid = 'current-date-' + Date.now();
+        div.innerHTML=`
+        <h4 class="text-2xl font-bold">${addingMoney} is donated for Flood Relief in Feni,Bangladesh</h4>
+        <p>Date:<span id="${uniqueid}"></span></p>
+        `
+        document.getElementById('history-container').appendChild(div);
+        document.getElementById(uniqueid).innerText = new Date().toString();
     }
     else{
         alert("Please Provide us your donate, not your words or funny number");
@@ -76,6 +109,16 @@ document.getElementById('button-clicker2').addEventListener('click',function(eve
     document.getElementById('total-donate2').innerHTML = `<a href="" id="total-donate2" class="pb-4 font-bold flex"><img class="pr-1" src="./images/coin.png" alt="">${finalDonate} BDT</a>`;
     document.getElementById('balance').innerHTML = `<p id="balance" class="flex items-center font-bold"><img class="pr-1" src="./images/coin.png" alt="">${remainingBalance} BDT</p>`;
     modal.classList.remove('hidden');
+
+    const div= document.createElement('div');
+        div.classList.add('border-solid','border-2','border-black','mb-4','p-3');
+        const uniqueid = 'current-date-' + Date.now();
+        div.innerHTML=`
+        <h4 class="text-2xl font-bold">${addingMoney} is Aided for Injured in the Quota Movement</h4>
+        <p>Date:<span id="${uniqueid}"></span></p>
+        `
+        document.getElementById('history-container').appendChild(div);
+        document.getElementById(uniqueid).innerText = new Date().toString();
 }
     else{
         alert("Please Provide us your donate, not your words or funny number");
